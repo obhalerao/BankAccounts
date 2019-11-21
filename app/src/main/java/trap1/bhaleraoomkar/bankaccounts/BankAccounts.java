0130fea72afcd3d2067777023146a4a4c79b8d53
obhalerao <obhal6543@gmail.com>
2019-11-21 09:38:10 -0500

    Added BankAccounts class

--- app/src/main/java/trap1/bhaleraoomkar/bankaccounts/BankAccounts.java
+++ app/src/main/java/trap1/bhaleraoomkar/bankaccounts/BankAccounts.java
@@ -7,6 +7,24 @@ public class BankAccounts {
     private BankAccount[] accs;
 
     public BankAccounts(){}
-    
+
+    public BankAccounts(int numAccs){
+        accs = new BankAccount[numAccs];
+        for(int i = 0; i < numAccs; i++){
+            accs[i] = new BankAccount("defaultName", (long)(Math.random()*1000000));
+        }
+    }
+
+    public void setBankAccount(BankAccount acc, int pos){
+        accs[pos] = acc;
+    }
+
+    public BankAccount[] getAccs(){
+        return accs;
+    }
+
+    public BankAccount getAcc(int pos){
+        return accs[pos];
+    }
 
 }
